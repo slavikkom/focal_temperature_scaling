26fb13e75d0f2a615ac9cc265f1feed6d8269917
Novin Shahroudi <novinsha@gmail.com>
2025-05-31 12:27:50 +0300

    Add script for parallel training on hpc for cifar10.

---
+++ train_scripts/cifar10_slurm_array.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#SBATCH --job-name=fts_cifar10
+#SBATCH --output=slurm_logs/job_%A_%a.out
+#SBATCH --array=0,1,2
+#SBATCH --partition=gpu
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1
+#SBATCH --time=00:05:00
+#SBATCH --mem=15G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=novin@ut.ee
+
+# Load models
+module purge
+# module load python/3.9.12
+# module load cudnn/8.0.5.39-11.1
+
+SAVE_PATH="../MODEL_DIRECTORY/CIFAR10/"
+SMOKE_ARG="--smoke_test" # to test that everything is running correctly. otherwise set it to empty string ""
+AMP_ARG="--amp" # run with automatic mixed precision enabled or not. If not set it to empty string ""
+
+case $SLURM_ARRAY_TASK_ID in
+  0)
+    ARGS="--loss cross_entropy"
+    ;;
+  1)
+    ARGS="--loss focal_loss --gamma 1.0"
+    ;;
+  2)
+    ARGS="--loss focal_loss --gamma 2.0"
+    ;;
+  3)
+    ARGS="--loss focal_loss --gamma 3.0"
+    ;;
+  4)
+    ARGS="--loss focal_loss --gamma 5.0"
+    ;;
+  5)
+    ARGS="--loss focal_loss --gamma 7.0"
+    ;;
+  6)
+    ARGS="--loss focal_loss_adaptive --gamma 3.0"
+    ;;
+  7)
+    ARGS="--loss adafocal"
+    ;;
+  *)
+    echo "Invalid array index $SLURM_ARRAY_TASK_ID"
+    exit 1
+    ;;
+esac
+
+python ../train.py --dataset cifar10 --model resnet50 $ARGS --decay 0.0005 -g $AMP --save-path $SAVE_PATH $SMOKE_ARG
