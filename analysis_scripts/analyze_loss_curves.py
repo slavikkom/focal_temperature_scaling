068d5c5dda660593ab533e41f78026fd34aa3a26
Novin Shahroudi <novinsha@gmail.com>
2025-06-04 13:37:38 +0300

    Update the script to plot loss curves for all model variations.
     - comparison with FP16 and FP32 and a reference result.
     - iterate over all model variations for each dataset and save the plot in results/figs.

--- analysis_scripts/analyze_loss_curves.py
+++ analysis_scripts/analyze_loss_curves.py
@@ -7,6 +7,10 @@ import matplotlib.pyplot as plt
 import re
 
 def parse_loss_file(filename):
+    if not os.path.exists(filename):
+        print(f"Warning: File '{filename}' does not exist. Skipping...")
+        return pd.Series()
+
     with open(filename, 'r') as f:
         content = f.read()
 
@@ -20,85 +24,96 @@ def parse_loss_file(filename):
         
     return pd.Series(losses).sort_index()
 
-dir_path = '../MODEL_DIRECTORY/hpc_results/CIFAR10/'
-# dir_path = 'MODEL_DIRECTORY/hpc_results/CIFAR100/'
-# dir_path = 'MODEL_DIRECTORY/old_results/CIFAR10/'
-# dir_path = 'MODEL_DIRECTORY/hpc_results/TINYIMAGENET/'
-# Parse each file
-model_name = "resnet50_cross_entropy" 
-# model_name = "resnet50_focal_loss_gamma_1.0" 
-# model_name = "resnet50_focal_loss_gamma_2.0" 
-# model_name = "resnet50_focal_loss_gamma_3.0" 
-# model_name = "resnet50_focal_loss_gamma_5.0" 
-# model_name = "resnet50_focal_loss_gamma_7.0" 
-# model_name = "resnet50_adafocal" 
-# model_name = "resnet50_focal_loss_adaptive_gamma_3.0" 
 
-# model_name = "resnet50_ti_cross_entropy" 
-# model_name = "resnet50_ti_focal_loss_gamma_1.0" 
-# model_name = "resnet50_ti_focal_loss_gamma_2.0" 
-# model_name = "resnet50_ti_focal_loss_gamma_3.0" 
-# model_name = "resnet50_ti_focal_loss_gamma_5.0" 
-# model_name = "resnet50_ti_focal_loss_gamma_7.0" 
-# model_name = "resnet50_ti_adafocal" 
-# model_name = "resnet50_ti_focal_loss_adaptive_gamma_3.0" 
-train_loss = parse_loss_file(os.path.join(dir_path, f"{model_name}_train_loss.json"))  # Replace with actual filename
-val_loss = parse_loss_file(os.path.join(dir_path, f"{model_name}_val_loss.json"))      # Replace with actual filename
-# val_err = parse_loss_file(os.path.join(dir_path, f"{model_name}_val_error.json"))      # Replace with actual filename
-test_loss = parse_loss_file(os.path.join(dir_path, f"{model_name}_test_loss.json"))    # Already uploaded
+def get_loss_curves_as_df(dir_path, dataset_name, model_name, include_validation=False, label=''):
 
-# Combine into DataFrame
-df = pd.DataFrame({
-    "Train": train_loss,
-    # "Validation": val_loss,
-    "Test": test_loss
-})
+    dir_path = os.path.join(dir_path, dataset_name)
 
-# Plotting
-plt.figure(figsize=(10, 6))
-for column in df.columns:
-    if column == 'Validation':
-        plt.plot(df.index, df[column], label=column)
+    # model_name = filenames[0]  
+    train_loss = parse_loss_file(os.path.join(dir_path, f"{model_name}_train_loss.json"))  # Replace with actual filename
+    val_loss = parse_loss_file(os.path.join(dir_path, f"{model_name}_val_loss.json"))      # Replace with actual filename
+    # val_err = parse_loss_file(os.path.join(dir_path, f"{model_name}_val_error.json"))      # Replace with actual filename
+    test_loss = parse_loss_file(os.path.join(dir_path, f"{model_name}_test_loss.json"))    # Already uploaded
+
+    # Combine into DataFrame
+    if include_validation:
+        df = pd.DataFrame({
+            f"Train {label}": train_loss,
+            f"Val {label}": val_loss,
+            f"Test {label}": test_loss
+        })
     else:
+        df = pd.DataFrame({
+            f"Train {label}": train_loss,
+            f"Test {label}": test_loss
+        })
+
+    return df
+
+# Select the dataset
+# dataset_name = 'CIFAR10'
+# dataset_name = 'CIFAR100'
+dataset_name = 'TINYIMAGENET'
+save_loc = "../RESULTS/figs/"
+
+# Parse each file
+if dataset_name[:12] == 'TINYIMAGENET':
+    filenames = [
+        'resnet50_ti_cross_entropy',
+        'resnet50_ti_focal_loss_gamma_1.0',
+        'resnet50_ti_focal_loss_gamma_2.0',
+        'resnet50_ti_focal_loss_gamma_3.0',
+        'resnet50_ti_focal_loss_gamma_5.0',
+        'resnet50_ti_focal_loss_gamma_7.0',
+        'resnet50_ti_adafocal',
+        'resnet50_ti_focal_loss_adaptive_gamma_3.0',
+    ]
+else:
+    filenames = [
+        'resnet50_cross_entropy',
+        'resnet50_focal_loss_gamma_1.0',
+        'resnet50_focal_loss_gamma_2.0',
+        'resnet50_focal_loss_gamma_3.0',
+        'resnet50_focal_loss_gamma_5.0',
+        'resnet50_focal_loss_gamma_7.0',
+        'resnet50_adafocal',
+        'resnet50_focal_loss_adaptive_gamma_3.0', 
+    ]
+
+include_fp32 = True  # Set to True if you want to include FP32 results
+compare_with_old_results = True
+
+for model_name in filenames:
+    df = get_loss_curves_as_df('../MODEL_DIRECTORY/hpc_results', dataset_name, model_name, label='(FP16)')
+    if include_fp32:
+        df_fp32 = get_loss_curves_as_df('../MODEL_DIRECTORY/hpc_results', dataset_name+"_FP32", model_name, label='(FP32)')
+
+    # compare with the results from ecai for the reproducibility check
+    if compare_with_old_results:
+        df_old = get_loss_curves_as_df('../MODEL_DIRECTORY/old_results', dataset_name, model_name, label='(before)')
+
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    
+    for column in df.columns: # FP16
         plt.plot(df.index, df[column], label=column)
 
-plt.xlabel("Epoch")
-plt.ylabel("Loss")
-plt.title(f"Training Curve ({model_name}) - {dir_path.split('/')[-2]}")
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+    if include_fp32 and df_fp32.size > 0: 
+        for column in df_fp32.columns: # FP32
+            plt.plot(df_fp32.index, df_fp32[column], label=column)
 
+    if compare_with_old_results and df_old.size > 0:
+        for column in df_old.columns: # FP32
+            plt.plot(df_old.index, df_old[column], label=column)
 
-#%% plot two loss curves against each other from different runs 
-dir_path = '../MODEL_DIRECTORY/old_results/CIFAR10/'
-model_name = "resnet50_cross_entropy" 
-train_loss_old = parse_loss_file(os.path.join(dir_path, f"{model_name}_train_loss.json"))  # Replace with actual filename
-val_loss_old = parse_loss_file(os.path.join(dir_path, f"{model_name}_val_loss.json"))      # Replace with actual filename
-# val_err = parse_loss_file(os.path.join(dir_path, f"{model_name}_val_error.json"))      # Replace with actual filename
-test_loss_old = parse_loss_file(os.path.join(dir_path, f"{model_name}_test_loss.json"))    # Already uploaded
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title(f"Training Curve ({model_name}) - {dataset_name}")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
 
-df_old = pd.DataFrame({
-    "Train (before)": train_loss_old,
-    # "Validation (before)": val_loss_old,
-    "Test (before)": test_loss_old
-})
-
-# Plotting
-plt.figure(figsize=(10, 6))
-for column in df.columns:
-    plt.plot(df.index, df[column], label=column)
-
-for column in df_old.columns:
-    plt.plot(df_old.index, df_old[column], label=column)
-
-
-plt.xlabel("Epoch")
-plt.ylabel("Loss")
-plt.title(f"Training Curve ({model_name}) - {dir_path.split('/')[-2]}")
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-plt.show()
-
+    if not os.path.exists(save_loc):
+        os.makedirs(save_loc)
+    plt.savefig(os.path.join(save_loc, f"loss_curve_{dataset_name}_{model_name}.png"))
+    plt.show()
