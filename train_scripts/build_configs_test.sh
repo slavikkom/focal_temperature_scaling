49ea25f479e7c5e5a87fd4f5834d7b0234c522d0
Novin Shahroudi <novin@ut.ee>
2025-10-07 14:28:55 +0300

    Add a build_config_test for train jobs testing.

---
+++ train_scripts/build_configs_test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+function define_seeds() {
+    local -n seeds_ref=$1
+    seeds_ref+=(42)
+}
+
+function build_configs() {
+    local -n configs_ref=$1
+    local -a gammas=(1) 
+    local -a betas=(1)
+
+    # Softmax
+    configs_ref+=("--loss cross_entropy")
+
+    # Focal and its variants
+    for g in "${gammas[@]}"; do
+        configs_ref+=("--loss focal_loss --gamma $g")
+        # configs_ref+=("--loss linear --gamma $g")
+        # configs_ref+=("--loss exp_p --gamma $g")
+        # configs_ref+=("--loss exp_1mp --gamma $g")
+        # configs_ref+=("--loss one_minus_power --gamma $g")
+        # configs_ref+=("--loss log_power --gamma $g")
+        # configs_ref+=("--loss focal_loss_adaptive --gamma $g")
+    done
+    # configs_ref+=("--loss adafocal")
+
+    # Generalized focal (grid over beta × gamma)
+    # for b in "${betas[@]}"; do
+    #     for g in "${gammas[@]}"; do
+    #         configs_ref+=("--loss generalized_focal --beta $b --gamma $g")
+    #     done
+    # done
+}
