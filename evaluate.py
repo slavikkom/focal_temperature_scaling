f3e0414f55592a6ec7662001adf8fe9977bf12f7
Novin Shahroudi <novin@ut.ee>
2026-04-07 23:10:18 +0300

    Update dataset root path in training and evaluation scripts

--- evaluate.py
+++ evaluate.py
@@ -76,7 +76,7 @@ models = {
 
 def parseArgs():
     default_dataset = 'cifar10'
-    dataset_root = './data'
+    dataset_root = '../Data/datasets'
     model = 'resnet50'
     save_loc = './'
     save_eval_loc = './'
--- train.py
+++ train.py
@@ -117,7 +117,7 @@ def loss_function_save_name(loss_function,
 
 def parseArgs():
     default_dataset = 'cifar10'
-    dataset_root = './data/'
+    dataset_root = '../Data/datasets/'
     train_batch_size = 128
     test_batch_size = 128
     learning_rate = 0.1
