a44a20e8a16ade1e9a66b192eaad29ff5492ea4a
Novin Shahroudi <novin@ut.ee>
2026-06-02 13:15:07 +0300

    Add new baseline loss functions and proper focal loss to the build script that is shared among all train scripts.

--- train_scripts/build_configs.sh
+++ train_scripts/build_configs.sh
@@ -9,13 +9,24 @@ function define_seeds() {
 
 function build_configs() {
     local -n configs_ref=$1
+    local -a labelsmoothing=(0.05 0.1 0.15)
     local -a gammas=(0.25 0.5 0.75 1 1.5 2 3 5 7) 
+    # local -a gammas=(2 3 5) 
     local -a betas=(0.25 0.5 0.75 1 1.5 2 3 5 7)
     # local -a gammas=(1) 
     # local -a betas=(1)
 
+    # Random loss
+    # configs_ref+=("--loss random_loss")
+
     # Softmax
     configs_ref+=("--loss cross_entropy")
+    configs_ref+=("--loss brier_score")
+
+    # CE with label smoothing
+    for ls in "${labelsmoothing[@]}"; do
+        configs_ref+=("--loss cross_entropy --label-smoothing $ls")
+    done
 
     # Focal and its variants
     for g in "${gammas[@]}"; do
@@ -26,6 +37,7 @@ function build_configs() {
         configs_ref+=("--loss one_minus_power --gamma $g")
         configs_ref+=("--loss log_power --gamma $g")
         # configs_ref+=("--loss focal_loss_adaptive --gamma $g")
+        configs_ref+=("--loss proper_focal_loss --gamma $g")
     done
     # configs_ref+=("--loss adafocal")
 
