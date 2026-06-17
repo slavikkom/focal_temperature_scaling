1589670ca3766ac5fdeda4a75f87738dac5aa03e
Novin Shahroudi <novin@ut.ee>
2026-06-17 10:10:48 +0300

    Refactor evaluation script to support multiple seeds and improve logging for saving logits and labels.

--- evaluate_scripts/save_logits_labels.sh
+++ evaluate_scripts/save_logits_labels.sh
@@ -19,55 +19,91 @@ if [ "$SMOKE_TEST" = true ]; then
     MODEL_EPOCH=1 # Use the model trained for 1 epoch for smoke test
 fi
 
-mkdir -p $SAVE_EVAL_PATH
+SEED_DIRS=(42 123 2023)
 
+#####
 
-SAVE_PATH="../MODEL_DIRECTORY/CIFAR10/" # path used to save the models to use in order to lead them for evaluation
-SAVE_EVAL_PATH="../RESULTS/CIFAR10_LOGITSLABELS/" # path to save the evaluation results
+# SAVE_PATH="../MODEL_DIRECTORY/CIFAR10/" # path used to save the models to use in order to lead them for evaluation
+# SAVE_EVAL_PATH="../RESULTS/CIFAR10_LOGITSLABELS/" # path to save the evaluation results
 
-CUDA_VISIBLE_DEVICES=0 python ../evaluate.py \
---dataset cifar10 \
---model resnet50 \
--log \
-$GPU_FLAG \
-$SMOKE_FLAG \
---save-path $SAVE_PATH \
---save-eval-path $SAVE_EVAL_PATH \
---saved_model_name resnet50_cross_entropy_$MODEL_EPOCH.model \
---save-train-logits \
---save-val-logits \
---save-test-logits 
+# mkdir -p $SAVE_EVAL_PATH
+# for SEED in "${SEED_DIRS[@]}"; do
+#     SEED_SAVE_PATH="${SAVE_PATH%/}/${SEED}/"
+#     SEED_SAVE_EVAL_PATH="${SAVE_EVAL_PATH%/}/${SEED}/"
+
+#     mkdir -p "$SEED_SAVE_EVAL_PATH"
+
+#     echo "Running evaluation for seed $SEED -> save-path: $SEED_SAVE_PATH"
+
+#     CUDA_VISIBLE_DEVICES=0 python ../evaluate.py \
+#     --dataset cifar10 \
+#     --model resnet50 \
+#     -log \
+#     $GPU_FLAG \
+#     $SMOKE_FLAG \
+#     --save-path "$SEED_SAVE_PATH" \
+#     --save-eval-path "$SEED_SAVE_EVAL_PATH" \
+#     --saved_model_name resnet50_cross_entropy_$MODEL_EPOCH.model \
+#     --save-train-logits \
+#     --save-val-logits \
+#     --save-test-logits \
+#     >> "$SEED_SAVE_EVAL_PATH/ce_cifar10_logitslabels.log"
+# done
+
+#####
 
 SAVE_PATH="../MODEL_DIRECTORY/CIFAR100/" # path used to save the models to use in order to lead them for evaluation
 SAVE_EVAL_PATH="../RESULTS/CIFAR100_LOGITSLABELS/" # path to save the evaluation results
 
-CUDA_VISIBLE_DEVICES=0 python ../evaluate.py \
---dataset cifar100 \
---model resnet50 \
--log \
-$GPU_FLAG \
-$SMOKE_FLAG \
---save-path $SAVE_PATH \
---save-eval-path $SAVE_EVAL_PATH \
---saved_model_name resnet50_cross_entropy_$MODEL_EPOCH.model \
---save-train-logits \
---save-val-logits \
---save-test-logits 
+mkdir -p $SAVE_EVAL_PATH
+for SEED in "${SEED_DIRS[@]}"; do
+    SEED_SAVE_PATH="${SAVE_PATH%/}/${SEED}/"
+    SEED_SAVE_EVAL_PATH="${SAVE_EVAL_PATH%/}/${SEED}/"
+
+    mkdir -p "$SEED_SAVE_EVAL_PATH"
+
+    echo "Running evaluation for seed $SEED -> save-path: $SEED_SAVE_PATH"
+
+    CUDA_VISIBLE_DEVICES=0 python ../evaluate.py \
+    --dataset cifar100 \
+    --model resnet50 \
+    -log \
+    $GPU_FLAG \
+    $SMOKE_FLAG \
+    --save-path "$SEED_SAVE_PATH" \
+    --save-eval-path "$SEED_SAVE_EVAL_PATH" \
+    --saved_model_name resnet50_cross_entropy_$MODEL_EPOCH.model \
+    --save-train-logits \
+    --save-val-logits \
+    --save-test-logits \
+    >> "$SEED_SAVE_EVAL_PATH/ce_cifar100_logitslabels.log"
+done
+
+#####
 
 SAVE_PATH="../MODEL_DIRECTORY/DERMAMNIST_LR05/" # path used to save the models to use in order to lead them for evaluation
 SAVE_EVAL_PATH="../RESULTS/DERMAMNIST_LR05_LOGITSLABELS/" # path to save the evaluation results
 
-CUDA_VISIBLE_DEVICES=0 python ../evaluate.py \
---dataset dermamnist \
---model resnet18 \
--log \
-$GPU_FLAG \
-$SMOKE_FLAG \
---save-path $SAVE_PATH \
---save-eval-path $SAVE_EVAL_PATH \
---saved_model_name resnet18_cross_entropy_$MODEL_EPOCH.model \
---save-train-logits \
---save-val-logits \
---save-test-logits 
+mkdir -p $SAVE_EVAL_PATH
+for SEED in "${SEED_DIRS[@]}"; do
+    SEED_SAVE_PATH="${SAVE_PATH%/}/${SEED}/"
+    SEED_SAVE_EVAL_PATH="${SAVE_EVAL_PATH%/}/${SEED}/"
 
+    mkdir -p "$SEED_SAVE_EVAL_PATH"
 
+    echo "Running evaluation for seed $SEED -> save-path: $SEED_SAVE_PATH"
+
+    CUDA_VISIBLE_DEVICES=0 python ../evaluate.py \
+    --dataset dermamnist \
+    --model resnet18 \
+    -log \
+    $GPU_FLAG \
+    $SMOKE_FLAG \
+    --save-path "$SEED_SAVE_PATH" \
+    --save-eval-path "$SEED_SAVE_EVAL_PATH" \
+    --saved_model_name resnet18_cross_entropy_$MODEL_EPOCH.model \
+    --save-train-logits \
+    --save-val-logits \
+    --save-test-logits \
+    >> "$SEED_SAVE_EVAL_PATH/ce_dermamnist_logitslabels.log"
+done
